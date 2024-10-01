172330f6bb0f1bcc1ca8338a91f0786d6b9fc9d8
Param Patani <parampatani@gmail.com>
2024-10-01 15:53:19 -0600

    Week 3 complete

--- app/page.js
+++ app/page.js
@@ -5,7 +5,8 @@ function Page () {
   return(
   <div>
   <h1>CPRG 306: Web Development 2 - Assignments</h1>
-  <Link href='./week-2'>Week-2</Link>
+  <Link href='./week-2'>Week-2</Link><br/>
+  <Link href='./week-3'>Week-3</Link><br/>
   </div>
   );
 }
