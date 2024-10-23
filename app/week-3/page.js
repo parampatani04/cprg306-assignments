0be577c8972cf6653f42f7c566ffadb17dc09f10
Param Patani <parampatani@gmail.com>
2024-10-23 00:03:30 -0600

    remove

--- app/week-3/page.js
+++
@@ -1,9 +0,0 @@
-import ItemList from "./item-list.js";
-export default function Page() {
-    return(
-        <main className="bg-slate-950">
-            <h1 className="text-3xl font-bold m-2">Shopping List</h1>
-            <ItemList />
-        </main>
-    )
-}
--- app/week-4/page.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import NewItem from './new-item';
-
-export default function Page() {
-    return(
-        <NewItem />
-    )
-}
